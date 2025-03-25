c35d35bc53ad4579e1a6f9b4d50e248f774c933a
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-03-25 21:10:35 +0500

    add confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
 import { useState } from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+
 function App() {
   const [dice, setDice] = useState(() => getAllRandom());
 
   function getAllRandom() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
+      // value: 5,
       isHeld: false,
       id: nanoid(),
     }));
   }
-  console.log(dice);
+  let gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
   function rollDice() {
-    setDice((oldDice) =>
-      oldDice.map((die) => ({ ...die, value: Math.ceil(Math.random() * 6) }))
+    if (!gameWon) {
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld
+            ? { ...die }
+            : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+      setDice(getAllRandom());
+    }
+  }
+
+  function hold(id) {
+    console.log(id);
+    setDice((old) =>
+      old.map((die) => (id === die.id ? { ...die, isHeld: !die.isHeld } : die))
     );
   }
   let elements = dice.map((element) => (
-    <Die value={element.value} key={element.id} isHeld={element.isHeld} />
+    <Die
+      value={element.value}
+      key={element.id}
+      id={element.id}
+      isHeld={element.isHeld}
+      hold={hold}
+    />
   ));
   return (
     <>
+      {gameWon ? <Confetti gravity={0.07} /> : null}
       <main className="container">
-        <div className="game">
-          {elements}
-
-          <button onClick={rollDice}>roll</button>
-        </div>
+        <div className="game">{elements}</div>
+        <button className="roll-dice" onClick={rollDice}>
+          {gameWon ? "New Game" : "Roll"}
+        </button>
       </main>
     </>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,17 @@
 export default function Die(props) {
+  let style = {
+    backgroundColor: props.isHeld ? `#59E391` : `white`,
+  };
   return (
     <>
-      <button>{props.value}</button>
+      <button
+        style={style}
+        onClick={() => {
+          props.hold(props.id);
+        }}
+      >
+        {props.value}
+      </button>
     </>
   );
 }
